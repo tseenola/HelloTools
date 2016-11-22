9006c8476838c82fc3dab04cf1802f640073d5de
helloJun <641380205@qq.com>
2016-11-22 15:17:56 +0800

    add add my rsl utils

---
+++ hellolibrary/src/main/java/tools/com/hellolibrary/hello_rsa/RSAUtils.java
@@ -0,0 +1,319 @@
+package tools.com.hellolibrary.hello_rsa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+
+/**
+ * @author beimenshisan
+ * @date 2014年8月22日 下午1:44:23
+ *                      关于android客户端和c++服务器进行rsa加密对接
+ * RSA为非对称加密算法，一般由服务器人员产生一对密钥，密钥包含公钥和私钥（产生密钥时候需要设置长度值，长度一般为1024的倍数
+ * ，不同的长度值可以对不同大小的字节数组进行加密，计算公式：密钥长度值/8-11 ，这个公式计算后的值就是这个长度的密钥所能加密的
+ * 最大明文字节数组长度。eg:密钥长度为1024，所能加密的明文字节数组不能大于 1024/8-11=117个字节，即明文通过getByte()后不能超过
+ * 117（如果有汉字，gbk占用两个字节，utf-i一个汉字占用三个字节，单纯使用getbytes()后的字节长度来判断是否超过117可能不准确）,超过
+ * 限制，加密的时候会抛出 ......to large 异常，解决办法是对数据进行分段加密,）。然后服务器人员会把公钥交给android客户端，
+ * 客户端采用公钥对数据进行加密，然后服务器端使用私钥对数据进行解密。
+ *
+ * 出现问题
+ * 1.客户端使用公钥加密后服务器端无法使用私钥解密出来，
+ * 可能原因，虽然两端使用的都是RSA加密，但是算法可能不一样，所以服务器和android 客户端都要采用
+ * Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding");才行
+ * 2.客户端和服务器采用了相同算法和密钥对还是无法相互解密
+ * 可能原因，android这边加密有自动填充，当加密明文转换为byte[] 不够 密钥长度/8时，android会
+ * 在前面或者填充0，所以可能导致服务器解不出来。
+ * 解决办法：
+ * 客户端生成一个buffer数组，长度正好为256  byte [] buffer = new byte [256];
+ * 将明文填充到buffer中，如果明文getByte()长度不足256,就使用 System.arrayCopy(明文.getByte(),0,buffer,0,明文.getByte().length);
+ * 3.客户端加载私钥时候报非法，无法生成 RSAPrivate,无法进行解密
+ * 解决办法：服务器生成的私钥必须为 pkcs8格式android才能加载
+ *
+ *规则:长度为1024的密钥加密最大可以传入256个字节，返回的密文也是256个字节
+ *
+ *
+ *
+ *
+ */
+public final class RSAUtils {
+    private static String RSA = "RSA";
+
+    /**
+     * 随机生成RSA密钥对(默认密钥长度为1024)
+     *
+     * @return
+     */
+    public static KeyPair generateRSAKeyPair() {
+        return generateRSAKeyPair(1024);
+    }
+
+    /**
+     * 随机生成RSA密钥对
+     *
+     * @param keyLength 密钥长度，范围：512～2048<br>
+     *                  一般1024
+     * @return
+     */
+    public static KeyPair generateRSAKeyPair(int keyLength) {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance(RSA);
+            kpg.initialize(keyLength);
+            return kpg.genKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 用公钥加密 <br>
+     * 每次加密的字节数，不能超过密钥的长度值减去11
+     *
+     * @param data   需加密数据的byte数据
+     * @param pubKey 公钥
+     * @return 加密后的byte型数据
+     */
+    public static byte[] encryptData(byte[] data, PublicKey publicKey) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding");
+            // 编码前设定编码方式及密钥
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            // 传入编码数据并返回编码结果
+            byte encryped [] = cipher.doFinal(data);
+            return encryped;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 通过公钥byte[](publicKey.getEncoded())将公钥还原，适用于RSA算法
+     *
+     * @param keyBytes
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     */
+    public static PublicKey getPublicKey(byte[] keyBytes) throws NoSuchAlgorithmException,
+            InvalidKeySpecException {
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+        return publicKey;
+    }
+
+    /**
+     * 用私钥解密
+     *
+     * @param encryptedData 经过encryptedData()加密返回的byte数据
+     * @param privateKey    私钥
+     * @return
+     */
+    public static byte[] decryptData(byte[] encryptedData, PrivateKey privateKey) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte encryped []= cipher.doFinal(encryptedData);
+            int i = 0;
+            return encryped;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 通过私钥byte[]将公钥还原，适用于RSA算法
+     *
+     * @param keyBytes
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     */
+    public static PrivateKey getPrivateKey(byte[] keyBytes) throws NoSuchAlgorithmException,
+            InvalidKeySpecException {
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+        return privateKey;
+    }
+
+    /**
+     * 使用N、e值还原公钥
+     *
+     * @param modulus
+     * @param publicExponent
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     */
+    public static PublicKey getPublicKey(String modulus, String publicExponent)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        BigInteger bigIntModulus = new BigInteger(modulus);
+        BigInteger bigIntPrivateExponent = new BigInteger(publicExponent);
+        RSAPublicKeySpec keySpec = new RSAPublicKeySpec(bigIntModulus, bigIntPrivateExponent);
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+        return publicKey;
+    }
+
+    /**
+     * 使用N、d值还原私钥
+     *
+     * @param modulus
+     * @param privateExponent
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     */
+    public static PrivateKey getPrivateKey(String modulus, String privateExponent)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        BigInteger bigIntModulus = new BigInteger(modulus);
+        BigInteger bigIntPrivateExponent = new BigInteger(privateExponent);
+        RSAPublicKeySpec keySpec = new RSAPublicKeySpec(bigIntModulus, bigIntPrivateExponent);
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+        return privateKey;
+    }
+
+    /**
+     * 从字符串中加载公钥
+     *
+     * @param publicKeyStr 公钥数据字符串
+     * @throws Exception 加载公钥时产生的异常
+     */
+    public static PublicKey loadPublicKey(String publicKeyStr) throws Exception {
+        try {
+            byte[] buffer = Base64Utils.decode(publicKeyStr);
+            KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此算法");
+        } catch (InvalidKeySpecException e) {
+            throw new Exception("公钥非法");
+        } catch (NullPointerException e) {
+            throw new Exception("公钥数据为空");
+        }
+    }
+
+    /**
+     * 从字符串中加载私钥<br>
+     * 加载时使用的是PKCS8EncodedKeySpec（PKCS#8编码的Key指令）。
+     *
+     * @param privateKeyStr
+     * @return
+     * @throws Exception
+     */
+    public static PrivateKey loadPrivateKey(String privateKeyStr) throws Exception {
+        try {
+            byte[] buffer = Base64Utils.decode(privateKeyStr);
+            // X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
+            KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+            return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此算法");
+        } catch (InvalidKeySpecException e) {
+            throw new Exception("私钥非法");
+        } catch (NullPointerException e) {
+            throw new Exception("私钥数据为空");
+        }
+    }
+
+    /**
+     * 从文件中输入流中加载公钥
+     *
+     * @param in 公钥输入流
+     * @throws Exception 加载公钥时产生的异常
+     */
+    public static PublicKey loadPublicKey(InputStream in) throws Exception {
+        try {
+            return loadPublicKey(readKey(in));
+        } catch (IOException e) {
+            throw new Exception("公钥数据流读取错误");
+        } catch (NullPointerException e) {
+            throw new Exception("公钥输入流为空");
+        }
+    }
+
+    /**
+     * 从文件中加载私钥
+     *
+     * @param keyFileName 私钥文件名
+     * @return 是否成功
+     * @throws Exception
+     */
+    public static PrivateKey loadPrivateKey(InputStream in) throws Exception {
+        try {
+            return loadPrivateKey(readKey(in));
+        } catch (IOException e) {
+            throw new Exception("私钥数据读取错误");
+        } catch (NullPointerException e) {
+            throw new Exception("私钥输入流为空");
+        }
+    }
+
+    /**
+     * 读取密钥信息
+     *
+     * @param in
+     * @return
+     * @throws IOException
+     */
+    private static String readKey(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String readLine = null;
+        StringBuilder sb = new StringBuilder();
+        while ((readLine = br.readLine()) != null) {
+            if (readLine.charAt(0) == '-') {
+                continue;
+            } else {
+                sb.append(readLine);
+                sb.append('\r');
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * 打印公钥信息
+     *
+     * @param publicKey
+     */
+    public static void printPublicKeyInfo(PublicKey publicKey) {
+        RSAPublicKey rsaPublicKey = (RSAPublicKey) publicKey;
+        System.out.println("----------RSAPublicKey----------");
+        System.out.println("Modulus.length=" + rsaPublicKey.getModulus().bitLength());
+        System.out.println("Modulus=" + rsaPublicKey.getModulus().toString());
+        System.out.println("PublicExponent.length=" + rsaPublicKey.getPublicExponent().bitLength());
+        System.out.println("PublicExponent=" + rsaPublicKey.getPublicExponent().toString());
+    }
+
+    public static void printPrivateKeyInfo(PrivateKey privateKey) {
+        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) privateKey;
+        System.out.println("----------RSAPrivateKey ----------");
+        System.out.println("Modulus.length=" + rsaPrivateKey.getModulus().bitLength());
+        System.out.println("Modulus=" + rsaPrivateKey.getModulus().toString());
+        System.out.println("PrivateExponent.length=" + rsaPrivateKey.getPrivateExponent().bitLength());
+        System.out.println("PrivatecExponent=" + rsaPrivateKey.getPrivateExponent().toString());
+
+    }
+
+}
